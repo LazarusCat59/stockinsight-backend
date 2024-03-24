f1e3e3e5c20b70d81e1e626933bf622ab8363f46
LazarusCat59 <zayedferosh@gmail.com>
2024-03-24 17:24:31 +0530

    Setup simple permissions

---
+++ restapi/permissions.py
@@ -0,0 +1,15 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+class AuditorOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        
+        return (request.user.role == 'ADT' | request.user.role == 'HOD')
+
+class CustodianOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        
+        return (request.user.role == 'CDN' | request.user.role == 'HOD')
