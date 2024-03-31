da6739eee6845ebc76c05878744a0790451d300a
LazarusCat59 <zayedferosh@gmail.com>
2024-03-31 13:02:28 +0530

    Edited and added views and urls
    Added views and corresponding urls to list, create, update and
    delete stocks, stocktypes and auditdetails. Also added url and
    view to search stocks and stocktypes by name.

--- restapi/urls.py
+++ restapi/urls.py
@@ -1,15 +1,23 @@
-from django.urls import path, include
+from django.urls import path, re_path, include
 from . import views
 
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views as drf_views
 
 router = DefaultRouter()
-router.register(r'stock', views.StockViewSet)
-router.register(r'stocktype', views.StockTypeViewSet)
-router.register(r'auditdetails', views.AuditDetailsViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
     path('api/login/', drf_views.obtain_auth_token),
+    re_path(r'api/stock/(?P<pk>[0-9]*)/', views.StockRUDView.as_view()),
+    re_path(r'api/stocktype/(?P<pk>[0-9]*)/', views.StockTypeRUDView.as_view()),
+    re_path(r'api/audit/(?P<pk>[0-9]*)/', views.AuditDetailsRUDView.as_view()),
+    re_path(r'api/stock_create/', views.StockCreateView.as_view()),
+    re_path(r'api/stocktype_create/', views.StockTypeCreateView.as_view()),
+    re_path(r'api/audit_create/', views.AuditDetailsCreateView.as_view()),
+    path('api/stock_list/', views.StockListView.as_view()),
+    path('api/stocktype_list/', views.StockTypeListView.as_view()),
+    path('api/auditdetails_list/', views.AuditDetailsListView.as_view()),
+    path('api/stock_search/', views.StockSearchView.as_view()),
+    path('api/stocktype_search/', views.StockTypeSearchView.as_view()),
 ]
--- restapi/views.py
+++ restapi/views.py
@@ -1,16 +1,79 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework import generics, authentication, views, status
+from rest_framework import permissions as drf_permissions
+from rest_framework.response import Response
+from . import permissions
 from . import models
 from . import serializers
 
-class StockViewSet(viewsets.ModelViewSet):
+class StockListView(generics.ListAPIView):
     queryset = models.Stock.objects.all()
     serializer_class = serializers.StockSerializer
 
-class StockTypeViewSet(viewsets.ModelViewSet):
+class StockTypeListView(generics.ListAPIView):
     queryset = models.StockType.objects.all()
     serializer_class = serializers.StockTypeSerializer
 
-class AuditDetailsViewSet(viewsets.ModelViewSet):
+class AuditDetailsListView(generics.ListAPIView):
     queryset = models.AuditDetails.objects.all()
     serializer_class = serializers.AuditDetailsSerializer
+
+class StockRUDView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Stock.objects.all()
+    serializer_class = serializers.StockSerializer
+    permission_classes = [ drf_permissions.IsAuthenticated, permissions.CustodianOrReadOnly ]
+
+class StockTypeRUDView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.StockType.objects.all()
+    serializer_class = serializers.StockTypeSerializer
+    permission_classes = [ drf_permissions.IsAuthenticated, permissions.CustodianOrReadOnly ]
+
+class AuditDetailsRUDView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.AuditDetails.objects.all()
+    serializer_class = serializers.AuditDetailsSerializer
+    permission_classes = [ drf_permissions.IsAuthenticated, permissions.AuditorOrReadOnly ]
+
+class StockCreateView(generics.CreateAPIView):
+    queryset = models.Stock.objects.all()
+    serializer_class = serializers.StockSerializer
+    permission_classes = [ drf_permissions.IsAuthenticated, permissions.CustodianOrReadOnly ]
+
+class StockTypeCreateView(generics.CreateAPIView):
+    queryset = models.StockType.objects.all()
+    serializer_class = serializers.StockTypeSerializer
+    permission_classes = [ drf_permissions.IsAuthenticated, permissions.CustodianOrReadOnly ]
+
+class AuditDetailsCreateView(generics.CreateAPIView):
+    queryset = models.AuditDetails.objects.all()
+    serializer_class = serializers.AuditDetailsSerializer
+    permission_classes = [ drf_permissions.IsAuthenticated, permissions.AuditorOrReadOnly ]
+
+class StockSearchView(views.APIView):
+    """
+    View used for searching stock with name parameter
+    """
+    def get(self, request, format=None):
+        if not request.GET.get("name"):
+            return Response({"detail":"name must be included in request"}, status=status.HTTP_400_BAD_REQUEST)
+
+        stocks = [serializers.StockSerializer(s).data for s in models.Stock.objects.all() if s.name.startswith(request.GET.get("name"))]
+
+        if(stocks):
+            return Response(stocks)
+        else:
+            return Response({"detail":"No stocks starting with given name found"}, status=status.HTTP_404_NOT_FOUND)
+
+class StockTypeSearchView(views.APIView):
+    """
+    View used for searching stock with name parameter
+    """
+    def get(self, request, format=None):
+        if not request.GET.get("name"):
+            return Response({"detail":"name must be included in request"}, status=status.HTTP_400_BAD_REQUEST)
+
+        stocktypes = [serializers.StockTypeSerializer(s).data for s in models.StockType.objects.all() if s.name.startswith(request.GET.get("name"))]
+
+        if(stocktypes):
+            return Response(stocktypes)
+        else:
+            return Response({"detail":"No stocks starting with given name found"}, status=status.HTTP_404_NOT_FOUND)
