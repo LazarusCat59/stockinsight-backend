ab54ee94a4a67828cf20f4bc89de58cd92557eb0
LazarusCat59 <zayedferosh@gmail.com>
2024-04-22 13:39:37 +0530

    Changed serializer to HyperlinkedModelSerializer

--- backend/urls.py
+++ backend/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('restapi.urls'))
+    path('', include('restapi.urls'), name='api')
 ]
--- restapi/serializers.py
+++ restapi/serializers.py
@@ -1,22 +1,32 @@
 from rest_framework import serializers
 from . import models 
 
-class StockSerializer(serializers.ModelSerializer):
+class StockSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Stock
         fields = '__all__'
+        extra_kwargs = {
+                'url': { 'view_name' : 'stock-detail' },
+                'type' : { 'view_name' : 'stocktype-detail' },
+        }
 
-class StockTypeSerializer(serializers.ModelSerializer):
+class StockTypeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.StockType
         fields = '__all__'
+        extra_kwargs = {
+                'url': { 'view_name' : 'stocktype-detail' },
+        }
 
-class AuditDetailSerializer(serializers.ModelSerializer):
+class AuditDetailSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.AuditDetail
         fields = '__all__'
+        extra_kwargs = {
+                'url': { 'view_name' : 'audit-detail' },
+        }
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.User
         fields = ('username', 'email', 'role')
